74aabe3f88b991c76b3b410e7f9b7d05185f63c9
h1amood2y <45898205+h1amood2y@users.noreply.github.com>
2018-12-15 23:24:15 +0200

    Create index-3.js

---
+++ index-3.js
@@ -0,0 +1,8 @@
+<script type='text/javascript'>
+var pageresults=soraOptions.postperPage;
+var numshowpage=3;
+var upPageWord=&#39;&lt;i class=&quot;fa fa-angle-double-left&quot;&gt;&lt;/i&gt;&#39;;
+var downPageWord=&#39;&lt;i class=&quot;fa fa-angle-double-right&quot;&gt;&lt;/i&gt;&#39;;
+var urlactivepage=location.href;
+var home_page=&quot;/&quot;;
+</script>
