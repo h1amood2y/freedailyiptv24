f9eba401808dc9d6ee14f42faa6e7e80669d51e1
h1amood2y <45898205+h1amood2y@users.noreply.github.com>
2018-12-15 16:57:31 +0200

    Create index-5.js

---
+++ index-5.js
@@ -0,0 +1,11 @@
+<script data-cfasync='false' type='text/javascript'> 
+ var _foxpush = _foxpush || []; 
+ _foxpush.push([&#39;_setDomain&#39;, &#39;korafreetvcom&#39;]); 
+ (function(){ 
+ var foxscript = document.createElement(&#39;script&#39;); 
+ foxscript.src = &#39;//cdn.foxpush.net/sdk/foxpush_SDK_min.js&#39;; 
+ foxscript.type = &#39;text/javascript&#39;; 
+ foxscript.async = &#39;true&#39;; 
+ var fox_s = document.getElementsByTagName(&#39;script&#39;)[0]; 
+ fox_s.parentNode.insertBefore(foxscript, fox_s);})(); 
+ </script>
